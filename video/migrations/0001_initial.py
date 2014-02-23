8b05cd8fa607eb2c59e003b90e45288d30b6769d
Mathieu Comandon <strycore@gmail.com>
2014-02-23 03:31:07 +0100

    Reset video migrations

--- video/migrations/0001_initial.py
+++ video/migrations/0001_initial.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -9,98 +9,100 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Profile'
-        db.create_table('video_profile', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'video_profile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
             ('size_quota', self.gf('django.db.models.fields.IntegerField')(default=52428800)),
             ('video_quota', self.gf('django.db.models.fields.IntegerField')(default=5)),
         ))
-        db.send_create_signal('video', ['Profile'])
+        db.send_create_signal(u'video', ['Profile'])
 
         # Adding model 'Video'
-        db.create_table('video_video', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'video_video', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('profile', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['video.Profile'])),
-            ('host', self.gf('django.db.models.fields.CharField')(max_length=64, null=True, blank=True)),
-            ('page_title', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
+            ('host', self.gf('django.db.models.fields.CharField')(max_length=64, blank=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=256, blank=True)),
             ('page_url', self.gf('django.db.models.fields.CharField')(max_length=256)),
             ('duration', self.gf('django.db.models.fields.IntegerField')(default=0)),
             ('filename', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('file_suffix', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('extension', self.gf('django.db.models.fields.CharField')(max_length=10)),
             ('state', self.gf('django.db.models.fields.CharField')(max_length=24)),
             ('thumbnail', self.gf('django.db.models.fields.files.FileField')(max_length=100, null=True)),
-            ('progress', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
+            ('progress', self.gf('django.db.models.fields.FloatField')(null=True)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('private', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal('video', ['Video'])
+        db.send_create_signal(u'video', ['Video'])
 
 
     def backwards(self, orm):
         # Deleting model 'Profile'
-        db.delete_table('video_profile')
+        db.delete_table(u'video_profile')
 
         # Deleting model 'Video'
-        db.delete_table('video_video')
+        db.delete_table(u'video_video')
 
 
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'video.profile': {
+        u'video.profile': {
             'Meta': {'object_name': 'Profile'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'size_quota': ('django.db.models.fields.IntegerField', [], {'default': '52428800'}),
-            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
             'video_quota': ('django.db.models.fields.IntegerField', [], {'default': '5'})
         },
-        'video.video': {
+        u'video.video': {
             'Meta': {'object_name': 'Video'},
             'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'duration': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'file_suffix': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'extension': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
             'filename': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'host': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'page_title': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'host': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'page_url': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'profile': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['video.Profile']"}),
-            'progress': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'private': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'profile': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['video.Profile']"}),
+            'progress': ('django.db.models.fields.FloatField', [], {'null': 'True'}),
             'state': ('django.db.models.fields.CharField', [], {'max_length': '24'}),
-            'thumbnail': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True'})
+            'thumbnail': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256', 'blank': 'True'})
         }
     }
 
